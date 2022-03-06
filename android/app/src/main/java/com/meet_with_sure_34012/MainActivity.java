656eff78935b31c9d7a3a01ba32708a4d4644f3e
Crowdbotics <team@crowdbotics.com>
2022-03-06 12:13:12 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/meet_with_sure_34012/MainActivity.java
@@ -0,0 +1,15 @@
+package com.meet_with_sure_34012;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "meet_with_sure_34012";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "meet_with_sure_34012",
+  url: "https://meet_with_sure_34012.botics.co",
+  api: "https://meet_with_sure_34012.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
